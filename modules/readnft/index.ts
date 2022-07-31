3acdc8ac7b1f8dd3412d2a9c3d4028f084c9de55
nabi.lee <hello@nabi.kim>
2022-07-31 21:40:14 +0900

    Add code template

--- modules/readnft/index.ts
+++ modules/readnft/index.ts
@@ -1,5 +1,33 @@
-import { TokenMeta, TokenVisible } from "constants/database";
+import { TokenMeta as Meta, TokenVisible as Visible } from "constants/database";
+import { Numeric } from "../types";
 
-export const mergeVisibleAndMeta = (visible: TokenVisible[]) => (meta: TokenMeta[]) => {
+export type Info = Visible &
+  Meta & {
+    _id: Numeric;
+  };
+export type NumericMap<T> = Map<Numeric, T>;
+export type MappedVisible = NumericMap<Visible>;
+export type MappedInfo = NumericMap<Info>;
+
+export const visiblesToMap: (x: Visible[]) => MappedVisible = (x) => new Map(x.map((x) => [x.nftTokenId, x]));
+
+export function mergeVisiblesAndMetas(map: MappedVisible) {
+  return (meta: Meta[]): MappedInfo => new Map();
+}
+
+export function getId(map: MappedVisible) {
+  return (id: Numeric): Visible => map.get(id) || visible(id);
+}
+
+export function visible(nftTokenId: Numeric): Visible {
+  return {
+    id: "-1",
+    nftTokenId,
+    order: "0",
+    isHidden: "false",
+  };
+}
+
+export const mergeVisibleAndMeta = (visible: Visible[]) => (meta: Meta[]) => {
   return [];
 };
