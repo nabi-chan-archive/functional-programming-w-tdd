41cb72b6c0ddf621bbe934e52b25d1fa2d4335ac
nabi.lee <hello@nabi.kim>
2022-08-07 12:35:51 +0900

    Delete unused logs

--- modules/utils/log.test.ts
+++
@@ -1,9 +0,0 @@
-import { log } from "./log";
-
-describe("logger", () => {
-  it("show logging", () => {
-    const TEST_KEY = "KEY";
-    const TEST_VALUE = "TEST";
-    expect(log(TEST_KEY)(TEST_VALUE)).toStrictEqual(TEST_VALUE);
-  });
-});
--- modules/utils/log.ts
+++
@@ -1,3 +0,0 @@
-import { tap } from "ramda";
-
-export const log = <T>(key: string) => tap<T>((...args) => console.log(`${key} :`, ...args));
